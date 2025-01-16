91a75e84091ee1cf607e84947e3e520b25e3c85b
Carmel Fred PATCHELLI <f.patchelli@gmail.com>
2025-01-16 14:42:51 +0100

    fix auth error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,13 +3,10 @@
 import AppTable from "@/components/shared/app-table";
 import { DeleteDialog } from "@/components/shared/delete-dialog";
 import { Card } from "@/components/ui/card";
-import { UserProvider, useUser } from "@/context/user-provider";
-import useUserStore from "@/context/user-store";
 import { useTheme } from "next-themes";
 import React, { useEffect } from "react";
 
 export default function Home() {
-  const { theme } = useTheme();
   const [isClient, setIsClient] = React.useState(false);
 
   useEffect(() => {
